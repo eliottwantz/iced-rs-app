3fb79996e822cedc3c88f5b5fa139ea5323cb652
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2024-01-16 14:04:07 +0100

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    println!("Let's start this shit");
+    Editor::run(Settings::default())
+}
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        "Iced Editor".into()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello iced!").into()
+    }
 }
